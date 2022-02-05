c6cc59a7d72b3ed92db5eb56e6ee3cb25ef6191a
YotaXP <yotaxp+git@gmail.com>
2022-02-05 00:09:47 -0500

    Adds SetProperty overloads that avoid allocation.

--- PTUDataEditor/ViewModels/ViewModelBase.cs
+++ PTUDataEditor/ViewModels/ViewModelBase.cs
@@ -11,6 +11,33 @@ public class ViewModelBase : INotifyPropertyChanged
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
+    protected void SetProperty<T>(ref T property, T value, string propertyName)
+    {
+        if (!Equals(property, value))
+        {
+            property = value;
+            OnPropertyChange(propertyName);
+        }
+    }
+    protected void SetProperty<T>(ref T property, T value, string propertyName1, string propertyName2)
+    {
+        if (!Equals(property, value))
+        {
+            property = value;
+            OnPropertyChange(propertyName1);
+            OnPropertyChange(propertyName2);
+        }
+    }
+    protected void SetProperty<T>(ref T property, T value, string propertyName1, string propertyName2, string propertyName3)
+    {
+        if (!Equals(property, value))
+        {
+            property = value;
+            OnPropertyChange(propertyName1);
+            OnPropertyChange(propertyName2);
+            OnPropertyChange(propertyName3);
+        }
+    }
     protected void SetProperty<T>(ref T property, T value, params string[] propertyNames)
     {
         if (!Equals(property, value))
